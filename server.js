2742dbb910dfdeeb3e8df000130ffe02144298c0
usain-bolt <gubaydullin-vildan@mail.ru>
2020-11-22 11:42:36 +0300

    changed port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
         : fs.readFileSync('./public/index.html')
     res.end(body)
 })
-
-server.listen(3000)
+const port = process.env.PORT || 3000
+server.listen(port)
 
 console.log('Server started')
