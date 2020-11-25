40e65aea6600a86ff8e1c127afd2ba4334a39a1b
usain-bolt <gubaydullin-vildan@mail.ru>
2020-11-25 17:30:57 +0300

    shop hw lesson3

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    const body = req.url === '/style.css' 
-        ? fs.readFileSync('./public/style.css')
-        : fs.readFileSync('./public/index.html')
+    const path = './public'
+    let body = null
+    try{
+        body = fs.readFileSync(`${path}${req.url}`)
+    }
+    catch(e){
+        console.log(e)
+        body = fs.readFileSync(`${path}/index.html`)
+    }
     res.end(body)
 })
 const port = process.env.PORT || 3000
