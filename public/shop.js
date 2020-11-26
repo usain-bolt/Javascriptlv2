5525e82a858e8dd4365fd96114c223530958530b
usain-bolt <gubaydullin-vildan@mail.ru>
2020-11-26 18:32:12 +0300

    lesson4 hw

--- public/shop.js
+++ public/shop.js
@@ -201,20 +201,38 @@ class GoodItemInCard extends GoodItem {
 
 class Form{
     constructor(){
-        renderForm()
-        renderBtn()
-        btnAction()
+        this.renderForm()
+        this.renderBtn()
+        this.btnAction()
     }
     renderForm(){
         const placeToHolder = document.querySelector('.footer')
-        if(footer){
+        if(placeToHolder){
             const form = document.createElement('form')
             placeToHolder.appendChild(form)
             const labelName = document.createElement('span')
+            labelName.innerText = 'Имя'
             form.appendChild(labelName)
             const name = document.createElement('input')
+            name.classList.add('input-name')
             name.type = 'text'
             form.appendChild(name)
+
+            const labelPhone = document.createElement('span')
+            labelPhone.innerText = 'Мобильный телефон'
+            form.appendChild(labelPhone)
+            const phone = document.createElement('input')
+            phone.classList.add('input-phone')
+            phone.type = 'text'
+            form.appendChild(phone)
+
+            const labelMail = document.createElement('span')
+            labelMail.innerText = 'Email'
+            form.appendChild(labelMail)
+            const mail = document.createElement('input')
+            mail.classList.add('input-mail')
+            mail.type = 'email'
+            form.appendChild(mail)
         }
     }
     renderBtn(){
@@ -230,23 +248,39 @@ class Form{
     btnAction(){
         const btn = document.querySelector('.btn-submit')
         if(btn){
-            btn.addEventListener('submit', (event) =>{
+            btn.addEventListener('submit', event =>{
                 event.preventDefault()
-                checkName()
-                checkPhone()
-                checkMail()
+                console.log('Сообщение после отлова')
+                validation()
             })
         }
     }
-    checkName(){
-        const name = document.querySelector('input')
+    validation(){
+        const name = document.querySelector('.input-name')
         const reName = /^[A-ZА-Я]{1,1}[a-zа-я]{0,}$/g
-        if(reName.test(name.value)){
-            console.log('Имя соотвествует шаблону')
-            return true
+        if(!reName.test(name.value)){
+            console.log('Имя не соотвествует шаблону')
+            name.style.border = '2px solid red'
+            return false
         }
-        else console.log('Имя не соотвествует шаблону')
-        return false
+
+        const phone = document.querySelector('.input-phone')
+        const rePhone = /^\+7\([0-9]{3}\)[0-9]{3}\-[0-9]{4}$/g
+        if(!rePhone.test(phone.value)){
+            console.log('Телефон не соотвествует шаблону')
+            phone.style.border = '2px solid red'
+            return false
+        }
+
+        const mail = document.querySelector('.input-mail')
+        const reMail = /\w{1,}(\@mail\.ru)$/g
+        if(!reMail.test(mail.value)){
+            console.log('E-mail не соотвествует шаблону')
+            mail.style.border = '2px solid red'
+            return false
+        }
+        
+        return true
     }
 
 }
